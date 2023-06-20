6f95e7c4c06a9ba4aafc5166ccb0d0c962e182ed
Evgeny Ovodkov <kefir4ek@gmail.com>
2023-06-20 19:31:13 +0300

    refactor(popup): delete popup template from App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import PopupWithForm from './components/PopupWithForm/PopupWithForm';
 
 function App() {
 	return (
@@ -19,8 +18,6 @@ function App() {
 					Learn React
 				</a>
 			</header>
-
-			<PopupWithForm />
 		</div>
 	);
 }

6611a18fa7484285bd4af1d3c25b4fa9606a45fc
Evgeny Ovodkov <kefir4ek@gmail.com>
2023-06-21 21:02:50 +0300

    feat(fonts): add fonts into project

--- src/components/PopupWithForm/PopupWithForm.js
+++ src/components/PopupWithForm/PopupWithForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './PopupWithForm.scss';
 
 function PopupWithForm({ title, name, children, isOpen, onClose, onSubmit }) {
 	function handleMouseDown(evt) {

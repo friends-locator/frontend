d681336aa077079f68820692479447dfdec26aba
Iris <irinaais90@gmail.com>
2023-06-26 17:00:07 +0300

    feat: component TrackingMap draft

---
+++ src/components/TrackingMap/TrackingMap.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+// import Map from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+export default function TrackingMap() {
+  return (
+    <div>вкапрол</div>
+  );
+}

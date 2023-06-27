335fc6738432d61325bc692a24995ad010fba558
Evgeniy Borovikov <evvlboro@gmail.com>
2023-06-27 14:58:16 +0300

    feat(signin page): add signin page

---
+++ src/pages/Signin/Signin.js
@@ -0,0 +1,142 @@
+import './Signin.scss';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import {
+	emailPattern,
+	emptyEmailErrorText,
+	emptyPasswordErrorText,
+	invalidEmailErrorText,
+	ROUTES,
+} from '../../constants';
+
+import { Button, InputText, InputPassword } from '../../components';
+
+export const Signin = () => {
+	const navigate = useNavigate();
+
+	const [userData, setUserData] = useState({
+		email: '',
+		password: '',
+	});
+
+	const [emailDirty, setEmailDirty] = useState(false);
+	const [passwordDirty, setPasswordDirty] = useState(false);
+
+	const [emailError, setEmailError] = useState(emptyEmailErrorText);
+	const [passwordError, setPasswordError] = useState(emptyPasswordErrorText);
+
+	const [passwordType, setPasswordType] = useState('password');
+
+	const handleChange = (evt) => {
+		const { name, value } = evt.target;
+
+		setUserData({
+			...userData,
+			[name]: value,
+		});
+
+		switch (name) {
+			case 'email':
+				if (String(value).length === 0) {
+					setEmailError(emptyEmailErrorText);
+				} else if (!value.match(emailPattern)) {
+					setEmailError(invalidEmailErrorText);
+				} else {
+					setEmailError('');
+				}
+				break;
+			case 'password':
+				if (String(value).length === 0) {
+					setPasswordError(emptyPasswordErrorText);
+				} else {
+					setPasswordError('');
+				}
+				break;
+			default:
+				break;
+		}
+	};
+
+	const blurHandler = (evt) => {
+		switch (evt.target.name) {
+			case 'email':
+				setEmailDirty(true);
+				break;
+			case 'password':
+				setPasswordDirty(true);
+				break;
+			default:
+				break;
+		}
+	};
+
+	const handlePasswordBtnClick = () => {
+		if (passwordType === 'password') {
+			setPasswordType('text');
+		} else {
+			setPasswordType('password');
+		}
+	};
+
+	const hadleBtnBackClick = () => {
+		navigate(-1);
+	};
+
+	return (
+		<section className="signin">
+			<div className="signin_container">
+				<button
+					type="button"
+					className="signin_btn-back"
+					onClick={hadleBtnBackClick}
+					aria-label="Back"
+				/>
+				<div className="signin_vector-1" />
+				<div className="signin_vector-2" />
+				<div className="signin_semicircle" />
+				<div className="signin_spiral" />
+				<form className="signin_form">
+					<h1 className="signin_form_title">С возвращением</h1>
+					<InputText
+						label="Email"
+						id="email"
+						name="email"
+						isRequired
+						inputValue={userData.email}
+						onChange={handleChange}
+						onBlur={blurHandler}
+						inputDirty={emailDirty}
+						inputError={emailError}
+					/>
+					<InputPassword
+						label="Придумайте пароль"
+						passwordType={passwordType}
+						id="password"
+						name="password"
+						isRequired
+						inputValue={userData.password}
+						onChange={handleChange}
+						onBlur={blurHandler}
+						passwordDirty={passwordDirty}
+						passwordError={passwordError}
+						onPasswordBtnClick={handlePasswordBtnClick}
+					/>
+					<Button
+						label="Войти"
+						type="submit"
+						color="primary"
+						size="large"
+						className="registration_form_btn-continue"
+					/>
+					<span className="signin_form_span">
+						Еще нет аккаунта?{' '}
+						<Link to={ROUTES.REGISTRATION} className="signin_form_link">
+							Зарегистрироваться
+						</Link>
+					</span>
+				</form>
+			</div>
+		</section>
+	);
+};

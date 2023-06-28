564d7ad0d376bb9506d852597f505be117ccd0bf
Iris <irinaais90@gmail.com>
2023-06-28 18:44:07 +0300

    feat: added draft component TrackingMap

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import { Routes } from '../../routes';
+import TrackingMap from "../TrackingMap/TrackingMap";
 
 function App() {
 	return (
 		<div className="page">
 			<Routes />
+			<TrackingMap />
 		</div>
 	);
 }
--- src/components/TrackingMap/TrackingMap.js
+++ src/components/TrackingMap/TrackingMap.js
@@ -1,9 +1,20 @@
 import * as React from 'react';
-// import Map from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './TrackingMap.css';
 
 export default function TrackingMap() {
   return (
-    <div>вкапрол</div>
+      <MapContainer className="trackingMap__mapContainer" center={[41.721098, 44.725121]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[41.721098, 44.725121]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
   );
 }

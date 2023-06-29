b06c79bd2e91a9a8b340f2b9091f4dfc520b27e0
Iris <irinaais90@gmail.com>
2023-06-29 23:36:48 +0300

    feat: added a hardcode to display the location of friends on the map

---
+++ src/components/TrackingMap/friendsLocation.js
@@ -0,0 +1,17 @@
+import avatar1 from '../../images/avatarman.png';
+import avatar2 from '../../images/avatarwoman.png';
+
+export const friendsLocation = [
+   {
+     id: 1,
+     avatar: avatar1,
+     name: "Архыз",
+     position: [55.729094, 37.558545]
+   },
+  {
+    id: 2,
+    avatar: avatar2,
+    name: "Челябинская область",
+    position: [55.729753, 37.561687]
+  }
+ ];

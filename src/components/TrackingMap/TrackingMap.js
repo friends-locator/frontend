eccb9130dcacd3e2cd02287c6ca5d352d9cc9d0e
Iris <irinaais90@gmail.com>
2023-06-29 15:59:19 +0300

    feat: added Marker on the map

--- src/components/TrackingMap/TrackingMap.js
+++ src/components/TrackingMap/TrackingMap.js
@@ -2,10 +2,14 @@ import * as React from 'react';
 import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './TrackingMap.css';
+import { Icon } from "leaflet";
+import avatar from '../../images/avatarwoman.png';
 
-//  TODO стартовые координаты брать из геолокации пользователя
-
-const position = [41.721098, 44.725121];
+const position = [55.729348, 37.560709]; //  TODO стартовые координаты брать из геолокации пользователя
+const userIcon = new Icon({ //  TODO аватарку брать из личного кабинета пользователя
+  iconUrl: avatar,
+  iconSize: [40, 48],
+})
 
 export default function TrackingMap() {
   return (
@@ -14,7 +18,9 @@ export default function TrackingMap() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position} />
+        <Marker
+          position={position}
+          icon={userIcon} />
       </MapContainer>
   );
 }

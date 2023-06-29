b6490fe1f92d91e98700f2b3d42defee0519f782
Evgeniy Borovikov <evvlboro@gmail.com>
2023-06-29 18:12:28 +0300

    fix(welcome page): move Welcome component into page & fix buttons to links

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as Button } from './Button/Button';
 export { default as PopupWithForm } from './PopupWithForm/PopupWithForm';
 export { default as InputText } from './InputText/InputText';
 export { default as InputPassword } from './InputPassword/InputPassword';
+export { default as Carousel } from './Carousel/Carousel';
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 import { Routes as RoutesDOM, Route } from 'react-router-dom';
-import { Registration, TermsOfUse, PrivacyPolicy, Signin } from '../pages';
+import {
+	Registration,
+	TermsOfUse,
+	PrivacyPolicy,
+	Signin,
+	Welcome,
+} from '../pages';
 import { ROUTES } from '../constants';
 
 export const Routes = () => (
 	<RoutesDOM>
+		<Route path={ROUTES.ROOT} element={<Welcome />} />
 		<Route path={ROUTES.REGISTRATION} element={<Registration />} />
 		<Route path={ROUTES.SIGN_IN} element={<Signin />} />
 		<Route path={ROUTES.PRIVACY_POLICY} element={<PrivacyPolicy />} />

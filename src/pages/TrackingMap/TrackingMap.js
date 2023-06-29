5d02a6e2f49722d73c9ee8c4f4527e59c60f57d6
Iris <irinaais90@gmail.com>
2023-06-30 01:12:02 +0300

    style: for Map, Header, leaflet-control-zoom

--- src/pages/TrackingMap/TrackingMap.js
+++ src/pages/TrackingMap/TrackingMap.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
-import './TrackingMap.css';
 import { Icon } from "leaflet";
-import avatar from '../../images/avatarwoman.png';
-import { friendsLocation } from "./friendsLocation";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer"; // TODO делать запрос к серверу для получения списка друзей, брать их координаты
+import './TrackingMap.css';
+import avatar from '../../images/avatarwoman.png';
+import { friendsLocation } from "./friendsLocation";
 
 const position = [55.729348, 37.560709]; //  TODO стартовые координаты пользователя брать из контекст провайдера?
 const userIcon = new Icon({ //  TODO аватарку брать из компонента, который отвечает за аватар пользователя в Profile?
@@ -27,7 +27,6 @@ export function TrackingMap() {
           position={position}
           icon={userIcon} />
         {friendsLocation.map(friend => {
-          console.log(friend.avatar);
           return (
             <Marker
               key={friend.id}

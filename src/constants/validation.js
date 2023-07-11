9d99b6946c4482bf59199ae8433e517c0349ab69
Evgeniy Borovikov <evvlboro@gmail.com>
2023-07-11 18:43:00 +0300

    fix(registration): add numbers in nickname

--- src/constants/validation.js
+++ src/constants/validation.js
@@ -1,6 +1,7 @@
 export const emailPattern =
 	/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
 export const namePattern = /^[a-яёa-z -]{2,30}$/i;
+export const nicknamePattern = /^[a-яёa-z0-9 -]{2,30}$/i;
 
 export const emptyNameErrorText = 'Имя не может быть пустым';
 export const emptySurnameErrorText = 'Фамилия не может быть пустой';

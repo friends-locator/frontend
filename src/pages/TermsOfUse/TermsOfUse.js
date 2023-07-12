1d48c115f925ec7636dd764bf5201e18e34eb5b1
Evgeniy Borovikov <evvlboro@gmail.com>
2023-07-12 13:40:45 +0300

    fix(terms-of-use & privacy-policy): add container to PrivacyPolicy & TermsOfUse

--- src/pages/TermsOfUse/TermsOfUse.js
+++ src/pages/TermsOfUse/TermsOfUse.js
@@ -10,22 +10,24 @@ export const TermsOfUse = () => {
 
 	return (
 		<section className="terms-of-use">
-			<div className="terms-of-use__header">
-				<button
-					type="button"
-					className="terms-of-use__btn-back"
-					onClick={hadleBtnBackClick}
-					aria-label="Back"
-				/>
-				<h2 className="terms-of-use__title">Условия использования</h2>
+			<div className="terms-of-use__container">
+				<div className="terms-of-use__header">
+					<button
+						type="button"
+						className="terms-of-use__btn-back"
+						onClick={hadleBtnBackClick}
+						aria-label="Back"
+					/>
+					<h2 className="terms-of-use__title">Условия использования</h2>
+				</div>
+				<p className="terms-of-use__description">
+					Политика конфиденциальности — это документ, описывающий, как
+					организация обрабатывает и защищает личную информацию своих
+					пользователей или клиентов. Она должна содержать информацию о том,
+					какие данные собираются, как они используются, кто имеет доступ к этим
+					данным и как они защищены от несанкционированного доступа или утечек.
+				</p>
 			</div>
-			<p className="terms-of-use__description">
-				Политика конфиденциальности — это документ, описывающий, как организация
-				обрабатывает и защищает личную информацию своих пользователей или
-				клиентов. Она должна содержать информацию о том, какие данные
-				собираются, как они используются, кто имеет доступ к этим данным и как
-				они защищены от несанкционированного доступа или утечек.
-			</p>
 		</section>
 	);
 };

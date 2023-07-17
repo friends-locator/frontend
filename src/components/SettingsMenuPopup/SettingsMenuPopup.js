b5bd322393ffefcf24deb9c1ff1f174baa5f06c1
Iris <irinaais90@gmail.com>
2023-07-17 21:50:51 +0300

    feat: add navigation

--- src/components/SettingsMenuPopup/SettingsMenuPopup.js
+++ src/components/SettingsMenuPopup/SettingsMenuPopup.js
@@ -1,7 +1,9 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import MenuPopup from '../MenuPopup/MenuPopup';
 import './SettingsMenuPopup.scss';
 import Checkbox from '../Checkbox/Checkbox';
+import { ROUTES } from '../../constants';
 
 function SettingsMenuPopup({
 	isOpen,
@@ -26,6 +28,27 @@ function SettingsMenuPopup({
 					isActiveOption={isActiveNightTheme}
 				/>
 			</div>
+			<section className="settingsMenuPopup__navigation">
+				<div className="settingsMenuPopup__link-container">
+					<Link className="settingsMenuPopup__link" to={ROUTES.COMING_SOON}>
+						Видимость на карте
+					</Link>
+					<Link className="settingsMenuPopup__link" to={ROUTES.COMING_SOON}>
+						Сменить пароль
+					</Link>
+				</div>
+				<div className="settingsMenuPopup__link-container">
+					<Link className="settingsMenuPopup__link" to={ROUTES.TERMS_OF_USE}>
+						Условия использования
+					</Link>
+					<Link className="settingsMenuPopup__link" to={ROUTES.PRIVACY_POLICY}>
+						Политика конфиденциальности
+					</Link>
+					<Link className="settingsMenuPopup__link" to={ROUTES.COMING_SOON}>
+						Удалить аккаунт
+					</Link>
+				</div>
+			</section>
 		</MenuPopup>
 	);
 }

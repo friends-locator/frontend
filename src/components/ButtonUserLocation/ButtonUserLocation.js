d09fa1c03f2b6f5787c8ab21c3794a4586dd40cd
Evgeniy Borovikov <evvlboro@gmail.com>
2023-07-18 14:46:21 +0300

    style(style): prettier changes

--- src/components/ButtonUserLocation/ButtonUserLocation.js
+++ src/components/ButtonUserLocation/ButtonUserLocation.js
@@ -3,23 +3,24 @@ import PropTypes from 'prop-types';
 import './ButtonUserLocation.scss';
 
 export default function ButtonUserLocation({ handleClick }) {
-  const props = {
-    handleClick
-  };
+	const props = {
+		handleClick,
+	};
 
-  return (
-    <button
-      className="buttonUserLocation"
-      type='button'
-      aria-label="Мое местоположение на карте"
-      onClick={ props.handleClick }/>
-  );
+	return (
+		<button
+			className="buttonUserLocation"
+			type="button"
+			aria-label="Мое местоположение на карте"
+			onClick={props.handleClick}
+		/>
+	);
 }
 
 ButtonUserLocation.propTypes = {
-  handleClick: PropTypes.func
+	handleClick: PropTypes.func,
 };
 
 ButtonUserLocation.defaultProps = {
-  handleClick: undefined
+	handleClick: undefined,
 };

12a85bf355b35be6ca7a7a3bade8c0ca1517eda1
Iris <irinaais90@gmail.com>
2023-07-26 19:33:38 +0300

    feat: add InputPassword

--- src/components/PopupDeleteAccount/PopupDeleteAccount.js
+++ src/components/PopupDeleteAccount/PopupDeleteAccount.js
@@ -6,19 +6,31 @@ import PopupWithForm from '../PopupWithForm/PopupWithForm';
 import Button from '../Button/Button';
 import { ROUTES } from '../../constants';
 import success from '../../images/icon-success.svg';
+import InputPassword from '../InputPassword/InputPassword';
 
 function PopupDeleteAccount({ isOpen, onClose, deleteAccount }) {
 	const [title, setTitle] = useState(
 		'Вы действительно хотите удалить свой профиль в «Где друзья»?'
 	);
+	const [password, setPassword] = useState('');
+	const [passwordType, setPasswordType] = useState('password');
 
-	const handleDeleteClick = () => {
+	const handleChange = (evt) => setPassword(evt.target.value);
+
+	const handleDeleteClick = () =>
 		setTitle('Для подтверждения действия введите свой пароль');
-	};
 
 	const handleConfirmClick = () => {
 		deleteAccount();
 		setTitle('');
+	};
+
+	const handlePasswordBtnClick = () => {
+		if (passwordType === 'password') {
+			setPasswordType('text');
+		} else {
+			setPasswordType('password');
+		}
 	};
 
 	return (
@@ -38,7 +50,6 @@ function PopupDeleteAccount({ isOpen, onClose, deleteAccount }) {
 					<p className="delete-account__text">Профиль удалён</p>
 				</div>
 			)}
-
 			{title ===
 				'Вы действительно хотите удалить свой профиль в «Где друзья»?' && (
 				<div className="delete-account__btn-container">
@@ -60,28 +71,39 @@ function PopupDeleteAccount({ isOpen, onClose, deleteAccount }) {
 					/>
 				</div>
 			)}
-
+			{/* TODO passwordDirty и passwordError использовать или убрать из обязательных у InputPassword */}
 			{title === 'Для подтверждения действия введите свой пароль' && (
-				<div className="delete-account__btn-container">
-					<Button
-						className="delete-account__btn"
-						label="Отмена"
-						type="button"
-						color="secondary"
-						size="medium"
-						onClick={onClose}
+				<>
+					<InputPassword
+						label="Пароль"
+						passwordType={passwordType}
+						id="password"
+						name="password"
+						isRequired
+						inputValue={password || ''}
+						onChange={handleChange}
+						onPasswordBtnClick={handlePasswordBtnClick}
 					/>
-					<Button
-						className="delete-account__btn"
-						label="Подтвердить"
-						type="button"
-						color="primary"
-						size="medium"
-						onClick={handleConfirmClick}
-					/>
-				</div>
+					<div className="delete-account__btn-container">
+						<Button
+							className="delete-account__btn"
+							label="Отмена"
+							type="button"
+							color="secondary"
+							size="medium"
+							onClick={onClose}
+						/>
+						<Button
+							className="delete-account__btn"
+							label="Подтвердить"
+							type="button"
+							color="primary"
+							size="medium"
+							onClick={handleConfirmClick}
+						/>
+					</div>
+				</>
 			)}
-
 			{title === '' && (
 				<Link to={ROUTES.ROOT}>
 					<Button
